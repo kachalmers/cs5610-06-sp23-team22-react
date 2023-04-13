7fca0447b74687c2b77ea5d3c2d048310d851826
Ada Onyekaba <62957076+adaonyekaba3@users.noreply.github.com>
2023-04-13 19:57:41 -0400

    adding login stuff

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,54 +1,71 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { loginThunk } from "../../services/users/users-thunks";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
-
-                <div className="mb-3">
-                    <label>Email address</label>
+function LoginScreen() {
+    const { currentUser } = useSelector((state) => state.users);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const login = async () => {
+        try {
+            await dispatch(loginThunk({ username, password }));
+            navigate("/profile");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return (
+        <form>
+            <h3>Sign In</h3>
+            <div className="form-group">
+                <label>Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={username}
+                    placeholder="Enter email"
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+            </div>
+            <div className="mb-3">
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+            </div>
+            <div className="mb-3">
+                <div className="custom-control custom-checkbox">
                     <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
+                        type="checkbox"
+                        className="custom-control-input"
+                        id="customCheck1"
                     />
+                    <label className="custom-control-label" htmlFor="customCheck1">
+                        Remember me
+                    </label>
                 </div>
-
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                    />
-                </div>
-
-                <div className="mb-3">
-                    <div className="custom-control custom-checkbox">
-                        <input
-                            type="checkbox"
-                            className="custom-control-input"
-                            id="customCheck1"
-                        />
-                        <label className="custom-control-label" htmlFor="customCheck1">
-                            Remember me
-                        </label>
-                    </div>
-                </div>
-
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
-                </div>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-                <div className={"Register New User bi-text-indent-right"}>
-                    Register <a href={"/sign-up"}>New Account</a>
-                </div>
-            </form>
-        )
-    }
+            </div>
+            <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </div>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+            <div className={"Register New User bi-text-indent-right"}>
+                Register <a href={"/sign-up"}>New Account</a>
+            </div>
+        </form>
+    );
 }
